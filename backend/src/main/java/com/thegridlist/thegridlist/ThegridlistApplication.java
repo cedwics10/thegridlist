03fac08903cc20a1f8407bbbb9bcea66d8020fd1
= <brochettes40@outlook.fr>
2025-05-19 21:09:35 +0200

    Package spring created

---
+++ backend/src/main/java/com/thegridlist/thegridlist/ThegridlistApplication.java
@@ -0,0 +1,13 @@
+package com.thegridlist.thegridlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThegridlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThegridlistApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thegridlist/thegridlist/ThegridlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegridlist.thegridlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThegridlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
